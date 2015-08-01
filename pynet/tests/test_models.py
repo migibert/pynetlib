ea343c0a50116fa1648ebe997dbd1450f2d825b0
Mikael Gibert <mikael.gibert@gmail.com>
2015-08-02 01:51:22 +0200

    Device initialization unit tests

--- pynet/tests/test_models.py
+++ pynet/tests/test_models.py
@@ -1,6 +1,7 @@
 import unittest
 from pynet.models import Namespace, Device
 
+
 class TestNamespace(unittest.TestCase):
     def test_init(self):
         name = 'mynamespace'
@@ -15,6 +16,25 @@ class TestNamespace(unittest.TestCase):
         self.assertEqual(ns.name, name)
         self.assertEqual(len(ns.devices), 0)
         self.assertTrue(ns.is_default())
+
+
+class TestDevice(unittest.TestCase):
+    def test_init(self):
+        id = '1'
+        name = 'eth0'
+        dev = Device(id, name)
+        self.assertEqual(dev.id, id)
+        self.assertEqual(dev.name, name)
+        self.assertFalse(dev.is_loopback())
+
+    def test_init_default(self):
+        id = '1'
+        name = 'lo'
+        dev = Device(id, name)
+        self.assertEqual(dev.id, id)
+        self.assertEqual(dev.name, name)
+        self.assertTrue(dev.is_loopback())
+
 
 if __name__ == '__main__':
     unittest.main()
