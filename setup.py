9cfde4b926dd9d0a3bd6761a5a5085d26b397206
Mikael Gibert <mikael.gibert@gmail.com>
2015-08-18 22:42:02 +0200

    pynetlib does not require any external dependency

--- setup.py
+++ setup.py
@@ -2,10 +2,6 @@
 Python net library
 """
 from setuptools import find_packages, setup
-from pip.req import parse_requirements
-
-install_reqs = parse_requirements('requirements.txt', session=False)
-dependencies = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='pynetlib',
@@ -19,5 +15,4 @@ setup(
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     zip_safe=False,
-    install_requires=dependencies,
 )
