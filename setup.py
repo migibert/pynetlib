bafa8771ce36de1bae898b206e160d3af2a15714
Mikael Gibert <mikael.gibert@numergy.com>
2015-08-20 15:38:22 +0200

    Releasing v0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pynetlib',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/migibert/pynetlib',
     license='MIT',
     author='Mikael Gibert',
