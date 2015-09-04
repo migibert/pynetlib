ec7f5aed9893d80cebd37cc39b023a7aeb7930c6
Mikael Gibert <mikael.gibert@numergy.com>
2015-09-04 14:05:21 +0200

    Preparing release 0.3.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ dependencies = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='pynetlib',
-    version='0.2.0',
+    version='0.3.0',
     url='https://github.com/migibert/pynetlib',
     license='MIT',
     author='Mikael Gibert',
